1140fd9e48a80c90e58d6c675541479c4b8711f1
Tiago Silva <tjcsilva04@gmail.com>
2023-08-20 17:43:20 +0100

    Solved B

---
+++ CSES/SortingAndBinarySearch/B.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<vector<int> > vv;
+typedef vector<vector<ll> > vvll;
+typedef vector<vector<pii> > vvpii;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+ll MOD = 1000000007;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF INT_MAX
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+int n,m,k,i,j,ans;
+
+bool good(int x,int target){
+	return abs(x-target) <= k;
+}
+
+void solve(){
+	ans = 0;
+	cin >> n >> m >> k;
+	vi p(n),aps(m);
+	forn(l,n){cin >> p[l];}
+	forn(l,m){cin >> aps[l];}
+	sort(all(p));sort(all(aps));
+	j = 0,i= 0;
+	while(i < n && j < m){
+		if (good(p[i],aps[j])){
+			ans++;
+			i++;
+			j++;
+		}
+		else if (p[i] > aps[j]){
+			j++;
+		}
+		else{
+			i++;
+		}
+
+
+	}
+	cout << ans << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ll t = 1;
+    //cin >> t;
+    for(int it=1;it<=t;it++) {
+        //cout << "Case #" << it+1 << ": ";
+        solve();
+    }
+    return 0;
+}
