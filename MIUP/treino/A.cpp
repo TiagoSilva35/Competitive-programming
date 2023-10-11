658448ea807c8669bde4a1bbe8590072afe43afc
TiagoSilva35 <tjcsilva04@gmail.com>
2023-10-11 18:33:37 +0100

    primeiro treino

---
+++ MIUP/treino/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+ll MOD = 1000000007;
+double eps = 1e-12;
+
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define INF INT_MAX
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve(){
+	map<int,int> m;	
+}
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ll t = 1;
+	//cin >> t;
+	for (int it = 1; it <= t; it++){
+		//cout << "Case #" << it + 1 << ": ";
+		solve();
+	}
+	return 0;
+}
