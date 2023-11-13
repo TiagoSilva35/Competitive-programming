d9e71d50a75d59f0bc50bcd29af7d07c505750bf
TiagoSilva35 <tjcsilva04@gmail.com>
2023-11-13 09:51:30 +0000

    stuff

--- MIUP/treino/TOPDEI#1/I.java
+++ MIUP/treino/TOPDEI#1/I.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 import java.util.*;
 
 public class I {
+    
+    /** 
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int n = sc.nextInt();
--- MIUP/treino/TOPDEI#7/A.java
+++ MIUP/treino/TOPDEI#7/A.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
 public class A {
+    
+    /** 
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
